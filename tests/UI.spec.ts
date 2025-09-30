899f382e7512c457f1506d1ab49fe00e3146fd16
Sam Roberts <samr72003@gmail.com>
2025-09-30 13:36:28 -0600

    feat: update coverage thresholds and enhance UI tests

--- tests/UI.spec.ts
+++ tests/UI.spec.ts
@@ -2,5 +2,9 @@ import { test, expect } from 'playwright-test-coverage';
 
 test('test', async ({ page }) => {
     await page.goto('http://localhost:5173/');
+    await expect(page.getByRole('navigation', { name: 'Global' })).toBeVisible();
+    await expect(page.locator('.w-screen')).toBeVisible();
+    await page.getByRole('button', { name: 'Order now' }).click();
+    await expect(page.locator('form div').first()).toBeVisible();
 });
 
